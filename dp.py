0b768fd43286adbacf1f827db6c2a70ba8c9339b
katsulol <69ferry69thoms69@gmail.com>
2024-06-23 01:31:57 +0530

    DATA SPLITTED INTO TRAINING AND TEST SET

--- dp.py
+++ dp.py
@@ -5,6 +5,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 
 df = pd.read_csv("Data.csv")
@@ -22,4 +23,10 @@ x[:, 1:3] = imputer.transform(x[:, 1:3])
 ct = ColumnTransformer(transformers =[("encoder", OneHotEncoder(), [0])], remainder = "passthrough")
 x = np.array(ct.fit_transform(x))
 y = LabelEncoder().fit_transform(y)
-print(x, y)
+#print(x, y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state= 1)
+#print(x_train)
+#print(x_test)
+#print(y_train)
+#print(y_test)
